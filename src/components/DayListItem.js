a382c53bbaf4559a9d9048902fde4acc37cc3958
Emma Grannis <emmagrannis@Emmas-MacBook-Pro.local>
2022-05-18 11:42:34 -0600

    Implement formatSpots function in DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,22 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const formatSpots = () => {
+    if (props.spots === 1) {
+      return (
+        `${props.spots} spot remaining`
+      )
+    }
+    else if (props.spots === 0) {
+      return (
+        "no spots remaining"
+      )
+    }
+  return (
+    `${props.spots} spots remaining`
+    )
+  }
+
   const listClass = classNames("day-list__item",
    {"day-list__item--selected": props.selected}, 
    {"day-list__item--full": !props.spots
@@ -11,7 +27,7 @@ export default function DayListItem(props) {
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 };
