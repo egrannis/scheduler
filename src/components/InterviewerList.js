3c404bacdecb9851411e3ac35a500b2dd4d19dff
Emma Grannis <emmagrannis@Emmas-MacBook-Pro.local>
2022-05-18 16:09:57 -0600

    Implement InterviewerList component, augment small bug in InterviewerListItem component, add styling for InterviewerList component, add InterviewerList component code to stories/index.js

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+//PROPS - interviewers is array of obj, setinterviewer accepts ID and comes from interviewlistitem, and interviewer is number that represents the id
+
+export default function InterviewerList(props) {
+  const {interviewers, setInterviewer, interviewer} = props;
+
+  const mappedInt = interviewers.map((item) => {
+    return (
+      <InterviewerListItem
+      key={item.id}
+      interviewerId={item.id}
+      name={item.name}
+      avatar={item.avatar}
+      selected={item.id === interviewer}
+      setInterviewer={setInterviewer}
+      />
+    )
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{mappedInt}</ul>
+    </section>  
+  );
+};
