bc5ce26988f83e30d880fe1df7e536ef293b76b2
Emma Grannis <emmagrannis@Emmas-MacBook-Pro.local>
2022-05-23 12:23:07 -0600

    Implement custom hook in Appointment/index.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -19,5 +19,5 @@ export default function useVisualMode(initial) {
     return setMode(history[history.length - 1]);
   }
 
-  return { mode, transition, back };
+  return { mode, transition, back }; // returns object with keys mode transition back with values of mode state, transition function, + back function. Employing object shorthand notation.
 }
