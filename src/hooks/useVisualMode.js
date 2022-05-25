d1f5310d7a041bbb6a64c05a88b86f5db2a8111d
Emma Grannis <emmagrannis@Emmas-MacBook-Pro.local>
2022-05-25 17:49:27 -0600

    Fix useVisualMode function and comment code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -7,18 +7,21 @@ export default function useVisualMode(initial) {
 
   function transition(next, replace = false) {
     if (replace) {
-      history.pop();
-      history.push(next);
-      return setMode(next);
+      setHistory((prev) => [...prev.slice(0, -1), next]);  // remove the last element and add the next mode as the final element
+      setMode(next);
     }
-    history.push(next);
-    // console.log(history);
-    return setMode(next);
+    setHistory((prev) => [...prev].push(next)); // if not replacing the history, simply append next to prev
   }
 
   function back() {
-    history.pop();
-    return setMode(history[history.length - 1]);
+    setHistory((prev) => {
+      if (prev.length === 1) { // if the length of our history is 1, that means that you can't go back further, so we just want to return that mode
+        return [...prev];
+      }
+
+      const historyBefore = [...prev.slice(0, -1)] // extracting what the history of modes was prior
+      return setMode(historyBefore[historyBefore.length - 1]); // set the mode as the last item in the past history array
+    })
   }
 
   return { mode, transition, back }; // returns object with keys mode transition back with values of mode state, transition function, + back function. Employing object shorthand notation.
